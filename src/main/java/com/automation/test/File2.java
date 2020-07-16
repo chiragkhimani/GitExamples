63602382ec7bbe39775f4f1d745f2af663082058
Urvi <urvipandya22@gmail.com>
2020-07-15 19:48:41 -0700

    Commit without pull

--- src/main/java/com/automation/test/File2.java
+++ src/main/java/com/automation/test/File2.java
@@ -3,6 +3,8 @@ package com.automation.test;
 public class File2 {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("Changes mad by Urvi before Pull");
+		System.out.println("Updating File2");
 
 }
 }
