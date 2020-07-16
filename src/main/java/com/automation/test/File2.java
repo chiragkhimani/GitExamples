89f38d015cdf09654b9bc7f0aca00845e6d2dfee
khimanichirag <khimanichirag@gmail.com>
2020-07-16 08:15:53 +0530

    Commit from Chirag

--- src/main/java/com/automation/test/File2.java
+++ src/main/java/com/automation/test/File2.java
@@ -3,6 +3,7 @@ package com.automation.test;
 public class File2 {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-
-}
+		System.out.println("Test message");
+		System.out.println("Test message2");
+	}
 }
